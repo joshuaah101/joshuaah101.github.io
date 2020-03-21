123920137c9dff50a14fe96d4a9408343f7caa30
Joshua <dejavaproglife@gmail.com>
2020-03-21 18:44:52 +0100

    I changed my mind to design a one page website

--- asset/js/josh.js
+++ asset/js/josh.js
@@ -14,10 +14,10 @@
       slides[index].classList.remove('active');
       
       //Go over each slide incrementing the index
-      index++;
+      ++index;
       
       // If you go over all slides, restart the index to show the first slide and start again
-      if (index === slides.length) index = 0; 
+      if (index >= slides.length) index = 0; 
       
       slides[index].classList.add('active');
 
